3716e61de9ae6ee876a8b0c73ea24a3463e2da19
severalfly <zhangsmile90@gmail.com>
2015-11-22 22:11:03 +0800

    java Arrays

---
+++ InitializationAndCleanup/Exercise16.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Exercise16
+{
+	public static void main(String[] args) {
+		
+	String[] strs = new String[]{
+		"zhang",
+		"yun",
+		"zheng",
+	};
+
+	for (String s: strs) {
+		System.out.println(s);
+	}
+	}
+}
