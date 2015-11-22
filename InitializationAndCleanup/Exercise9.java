7486790a930ccb0709b3cb87a5e09a41b0aaae0f
severalfly <zhangsmile90@gmail.com>
2015-11-22 16:31:44 +0800

    构造函数调用

--- InitializationAndCleanup/Exercise9.java
+++ InitializationAndCleanup/Exercise9.java
@@ -1,4 +1,19 @@
 public class Exercise9
 {
-	
+	int a = 0;
+	public Exercise9()
+	{
+		this(5);
+		// a = 5;
+	}
+	public Exercise9(int i)
+	{
+		this.a = i;
+	}
+
+
+	public static void main(String[] args) {
+		Exercise9 e = new Exercise9(5);
+		System.out.println(e.a);
+	}
 }
