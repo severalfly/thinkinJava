8e007f59b096593ab40bd1a75faae89f8aaa4797
张云飞 <zhangsmile90@gmail.com>
2018-05-18 15:33:28 +0800

    change default source directory

--- src/main/java/com/leon/App.java
+++ src/main/java/com/leon/App.java
@@ -1,5 +1,8 @@
 package com.leon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        List<String> list = new ArrayList<>();
+        list.add("aaa");
+        for (String str : list)
+        {
+            System.out.println(str);
+        }
     }
 }
