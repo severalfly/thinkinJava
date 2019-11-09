f86aded424ae87eab7ae8193afccf4d3430d7056
张云飞 <zhangsmile90@gmail.com>
2019-11-09 20:53:46 +0800

    删除过期代码

--- src/main/java/com/leon/constant/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.leon.constant;
-
-public class Constants {
-    // chang to your own project path
-    public static final String PATH_PRE = "/Users/zhangyunfei/work/openlearn/thinkinJava/";
-}
